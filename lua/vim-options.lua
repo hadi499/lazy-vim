24be69065849891ba7eae8f1d4d9d9810a5d9a25
hadi499 <hadi.ygcl@gmail.com>
2024-01-31 06:55:11 +0700

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,17 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
